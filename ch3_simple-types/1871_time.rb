8ec293203a02c1b21d49e15e5759fa721ff63785
Kevin Musiorski <musiorski@gmail.com>
2012-08-09 14:52:52 -0500

    whipped up little program to calculate time that 1871 has been open

--- ch3_simple-types/1871_time.rb
+++ ch3_simple-types/1871_time.rb
@@ -1,5 +1,18 @@
-# GrandOpening1871 = Time.new(2012, "may", 2, 10)
-# puts GrandOpening1871
+GrandOpening1871 = Time.new(2012, "may", 3, 10)
 
-puts "what is your name?"
-input = gets
+def timestamp_pretty(t)
+  "#{t.month}-#{t.day}-#{t.year}"
+end
+
+def time_diff_calc(t_now, t_then)
+  days = t_now.yday - t_then.yday
+  hours = t_now.hour - t_then.hour
+  "#{days} days and #{hours} hours"
+
+end
+
+puts "Today is #{timestamp_pretty(Time.now)}"
+
+puts "1871 opened on #{timestamp_pretty(GrandOpening1871)}"
+
+puts "1871 has been open for #{time_diff_calc(Time.now, GrandOpening1871)}"
