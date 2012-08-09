2476b5b4439fc7aa383bd31d3cdb3e183e99aa5a
Kevin Musiorski <musiorski@gmail.com>
2012-08-09 15:51:41 -0500

    refactor

--- ch3_simple-types/1871_time.rb
+++ ch3_simple-types/1871_time.rb
@@ -7,12 +7,11 @@ end
 def time_diff_calc(t_now, t_then)
   days = t_now.yday - t_then.yday
   hours = t_now.hour - t_then.hour
-  "#{days} days and #{hours} hours"
-
+  "#{days} days (and #{hours} hours)"
 end
 
-puts "Today is #{timestamp_pretty(Time.now)}"
+puts Time.now.strftime("Today is %m-%d-%y")
 
-puts "1871 opened on #{timestamp_pretty(GrandOpening1871)}"
+puts GrandOpening1871.strftime("1871 opened on %m-%d-%y")
 
 puts "1871 has been open for #{time_diff_calc(Time.now, GrandOpening1871)}"
