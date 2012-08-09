321ab70af8fe625297ea5e942dba3883a3c5b9c7
Kevin Musiorski <musiorski@gmail.com>
2012-08-09 11:23:17 -0500

    testing new default editor in git config

--- ch3_simple-types/1871_time.rb
+++ ch3_simple-types/1871_time.rb
@@ -1,2 +1,5 @@
-GrandOpening1871 = Time.new(2012, "may", 2, 10)
-puts GrandOpening1871
+# GrandOpening1871 = Time.new(2012, "may", 2, 10)
+# puts GrandOpening1871
+
+puts "what is your name?"
+input = gets
